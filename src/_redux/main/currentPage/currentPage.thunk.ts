665d7a1e3b37c8fa50b88113254888d8ea185f17
atulbhatt-system32 <atulbhatt98@gmail.com>
2025-03-13 16:36:44 +0530

    implement logic to manage initial expanded nodes and selected nodes in currentPage thunk; add setHoveredNodeUidThunk for hover state management

--- src/_redux/main/currentPage/currentPage.thunk.ts
+++ src/_redux/main/currentPage/currentPage.thunk.ts
@@ -7,8 +7,10 @@ import {
   removeExpandedNodeUid,
   setCurrentPageNewNodeTree,
   setCurrentPagePreviewContent,
+  setHoveredNodeUid,
 } from "./currentPage.slice";
 import { TreeStructure } from "@src/types/html.types";
+import htmlService from "@src/services/html.service";
 
 export const setCurrentPageNewNodeTreeThunk = createAsyncThunk(
   "currentPage/setCurrentPageNewNodeTree",
@@ -16,11 +18,43 @@ export const setCurrentPageNewNodeTreeThunk = createAsyncThunk(
     {
       nodeTree,
       previewContent,
-    }: { nodeTree: TreeStructure; previewContent: string },
+    }: {
+      nodeTree: TreeStructure;
+      previewContent: string;
+    },
     { dispatch },
   ) => {
     dispatch(setCurrentPageNewNodeTree(nodeTree));
+
+    // find uids of html and body node from the nodeTree to set initial expanded nodes
+    const initialExpandedNodes =
+      htmlService.getInitialExpandedNodesFromNodeTree(nodeTree);
+    dispatch(setExpandedNodeUids(initialExpandedNodes));
+
     dispatch(setCurrentPagePreviewContent(previewContent));
+
+    // if the initial expanded nodes include body, select the first child of body
+    // otherwise, select the body node
+    let bodyFound = false;
+    for (const node of initialExpandedNodes) {
+      if (node.toLowerCase().startsWith("body")) {
+        if (nodeTree[node].children.length > 0) {
+          const firstChild = nodeTree[node].children[0];
+          dispatch(setSelectedNodeUids([firstChild]));
+        } else {
+          dispatch(setSelectedNodeUids([node]));
+        }
+        bodyFound = true;
+        break; // Break the loop once body is found
+      }
+    }
+
+    // If body wasn't found, you might want to select a default node
+    if (!bodyFound && initialExpandedNodes.length > 0) {
+      dispatch(setSelectedNodeUids([initialExpandedNodes[0]]));
+    }
+
+    htmlService.scrollToElement(initialExpandedNodes.at(-1) as string);
   },
 );
 
@@ -28,6 +62,16 @@ export const setSelectedNodeUidsThunk = createAsyncThunk(
   "currentPage/setSelectedNodeUids",
   async (nodeUids: string[], { dispatch }) => {
     dispatch(setSelectedNodeUids(nodeUids));
+    htmlService.markSelectedElements(nodeUids);
+    htmlService.scrollToElement(nodeUids.at(-1) as string);
+  },
+);
+
+export const setHoveredNodeUidThunk = createAsyncThunk(
+  "currentPage/setHoveredNodeUid",
+  async (nodeUid: string, { dispatch }) => {
+    dispatch(setHoveredNodeUid(nodeUid));
+    htmlService.markHoveredElement(nodeUid);
   },
 );
 
