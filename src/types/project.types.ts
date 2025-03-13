509f29a39507a8a3d71c8f4d2d50e787a4615820
atulbhatt-system32 <atulbhatt98@gmail.com>
2025-03-13 16:38:51 +0530

    add new types for file and node structures in project.types.ts; enhance type definitions for better clarity and type safety

--- src/types/project.types.ts
+++ src/types/project.types.ts
@@ -1,5 +1,3 @@
-import { TFileNodeTreeData } from "@src/api";
-
 export type ProjectEventType = "reloadPage" | "openFile";
 
 export interface openFileEventData {
@@ -19,4 +17,64 @@ type ProjectEventDataMap = {
 export interface ProjectEvent<T extends ProjectEventType = ProjectEventType> {
   type: T;
   data: ProjectEventDataMap[T];
+}
+
+export type TNodeUid = string;
+export type TBasicNodeData = {
+  valid: boolean;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type TNodeData = TBasicNodeData & { [propName: string]: any };
+export type TNode = {
+  uid: TNodeUid;
+  parentUid: TNodeUid | null;
+
+  displayName: string;
+
+  isEntity: boolean;
+  children: TNodeUid[];
+
+  data: TNodeData;
+
+  uniqueNodePath?: string;
+};
+
+export type TFileNodeData = TBasicNodeData & {
+  path: string;
+
+  kind: "file" | "directory";
+  name: string;
+  ext: string;
+
+  orgContent: string;
+  content: string;
+  contentInApp?: string;
+
+  changed: boolean;
+};
+
+export type TFileNode = TNode & {
+  data: TFileNodeData;
+};
+
+export type TFileNodeTreeData = {
+  [uid: TNodeUid]: TFileNode;
+};
+export interface FilerStats {
+  dev: string;
+  node: string;
+  type: string;
+  size: number;
+  nlinks: number;
+  atime: string;
+  mtime: string;
+  ctime: string;
+  atimeMs: number;
+  mtimeMs: number;
+  ctimeMs: number;
+  mode: number;
+  uid: number;
+  gid: number;
+  name: string;
 }
